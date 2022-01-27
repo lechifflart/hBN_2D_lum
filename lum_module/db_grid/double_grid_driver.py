1094a41819891c73731403465ebd1a100f2ca2ce
lechifflart <pierre.lechifflart@gmail.com>
2022-01-27 15:54:47 +0100

    changed the matdyn parser ; still a source of errors : check TODO

--- lum_module/db_grid/double_grid_driver.py
+++ lum_module/db_grid/double_grid_driver.py
@@ -12,24 +12,10 @@ np.set_printoptions(threshold=sys.maxsize)
 # parse matdyn output ( do we launch matdyn jobs from here ?)
 
 # parse ypp output with interpolated grid 108x108x1, for all 20 excitons
-        # check if each grid is the same
 
-# expand IBZ exciton energies to FBZ (expand_kpoints from yambopy)
-        # need the list of symmetries
 list_of_points_to_expand, list_of_exc_nrgs = ypp_output_parser('/home/lechifflart/hBN_2D/test_code/o.exc-1-4_interpolated_IBZ_testgrid',[1,9])
-lat = YamboLatticeDB.from_db_file(filename='SAVE/ns.db1')
-#rec_sym = lat.sym_rec
-rec_red_sym = lat.sym_rec_red
 
-expanded_qtilde = expand_kpts(list_of_points_to_expand,rec_red_sym)
-expanded_qtilde_idx = np.array(expanded_qtilde)[:,0]
-expanded_qtilde_coord = np.array(expanded_qtilde)[:,1]
 
-# discard the duplicates in the expanded points
-new_array = [tuple(row.round(decimals=4)) for row in expanded_qtilde_coord]
-uniques = np.unique(new_array,axis=0)
-print(len(uniques))
-print("uniques",uniques)
 
 # visualize which points are selected
 car_uniques = red_car(uniques,lat.lat)
--- lum_module/db_grid/matdyn_freq_parser.py
+++ lum_module/db_grid/matdyn_freq_parser.py
@@ -2,13 +2,30 @@ import numpy as np
 
 def matdyn_freq_parser(file):
     # get a the q point and corresponding frequencies from QE matdyn, in cm-1
-    q_tilde = []
-    frequencies = []
     with open(file,'r') as f:
         lines=f.readlines()
-    for i in range(1,len(lines),2):
-        q_tilde.append(np.array([float(x) for x in lines[i].split()]))
-        frequencies.append(np.array([float(x) for x in lines[i+1].split()]))
-    return(q_tilde,frequencies)
 
-#print(matdyn_freq_parser('matdyn_dbg.freq'))
+    # extract nb of phonons modes and nb of k pts
+    nb_modes_k = [int(s) for s in lines[0].replace(',',' ').split() if s.isdigit()]
+
+    # loop for every point. Frequencies are 6 by line
+    q_tilde = np.empty([nb_modes_k[1],3])
+    frequencies = np.empty([nb_modes_k[1],nb_modes_k[0]])
+
+    step = -(nb_modes_k[0]//-6)  # ceiling division ; frequencies are at most 6 by line
+    print(step)
+    for i in range(1,len(lines)-step,step+1):
+        q_index = i//(step+1)
+        q_tilde[q_index]=np.array([float(x) for x in lines[i].split()])
+        #print(np.ravel(np.array([line.split() for line in lines[i+1:i+step+1]],dtype=object)))
+        frequencies[q_index] = np.array([float(x) for x in np.ravel([line.split() for line in lines[i+1:i+step+1]])])
+
+    # check if all points were parsed
+    if len(q_tilde)==nb_modes_k[1]:
+        return(q_tilde,frequencies)
+    else :
+        raise ValueError('[ERROR] wrong number of parsed kpoints in matdyn output')
+
+#matdyn_freq_parser('matdyn_dbg.freq')
+matdyn_freq_parser('test_other_matdyn_output')
+#matdyn_freq_parser('another_matdyn_output_test')
